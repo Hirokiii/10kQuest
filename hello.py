68bbd6705f2a614f0a3c54960263bcb82236897b
Hiroki Kaminaga <t1310352.hiroki.kaminaga@gmail.com>
2020-02-29 13:38:27 +0200

    test

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! <h2>Hello world!</h2>"
+    return "Hello! <h2>Hello world!</h2> Happy Mappy"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000, debug= True)
